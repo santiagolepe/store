ec5a0332076ba61455ea0e037d368ef1876d904b
Mario A Chavez <mario.chavez@gmail.com>
2013-01-26 13:24:59 -0600

    Implement new product and links for new and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,18 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new active: true
+  end
+
+  def create
+    @product = Product.new params[:product]
+
+    if @product.valid?
+      @product.save
+      return redirect_to products_path, notice: "Your product #{@product.name} has been created!"
+    end
+   render :new
+  end
 end
