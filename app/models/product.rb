432c6897b2401b21e643d9ca46f5c41bd65f7df8
octavio <santiagolepe@gmail.com>
2013-01-27 18:30:30 -0600

    Sistema rails

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,21 @@
 class Product < ActiveRecord::Base
-  before_save :set_defaults
-
-  belongs_to :category
-
   attr_accessible :active, :description, :name, :price, :category_id
 
-  validates :name, :category_id, presence: true
+  before_save :set_defaults
 
+  #relations
+  belongs_to :category
+  has_many :variants, :dependent => :destroy
+
+
+
+  #validations
+  validates :name, :category_id, presence: true
+  validates :price, :presence => true,
+            :numericality => { :only_integer => true, :greater_than => 0 }
+
+
+  #methods
   def set_defaults
     self.active ||= false
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130126204442) do
+ActiveRecord::Schema.define(:version => 20130127164049) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -28,5 +28,17 @@ ActiveRecord::Schema.define(:version => 20130126204442) do
     t.datetime "updated_at",  :null => false
     t.integer  "category_id"
   end
+
+  create_table "variants", :force => true do |t|
+    t.string   "color"
+    t.string   "size"
+    t.string   "nationality"
+    t.integer  "product_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "image"
+  end
+
+  add_index "variants", ["product_id"], :name => "index_variants_on_product_id"
 
 end
